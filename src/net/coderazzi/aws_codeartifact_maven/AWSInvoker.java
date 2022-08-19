a77cde3c80cf77ad50ba9735d582086bfb59e219
coderazzi <lu@coderazzi.net>
2022-08-19 18:35:47 +0200

    AWSInvoker corrections

--- src/net/coderazzi/aws_codeartifact_maven/AWSInvoker.java
+++ src/net/coderazzi/aws_codeartifact_maven/AWSInvoker.java
@@ -1,7 +1,6 @@
 package net.coderazzi.aws_codeartifact_maven;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.concurrent.TimeUnit;
@@ -17,7 +16,7 @@ class AWSInvoker {
         OperationOutput ret = new OperationOutput();
         try {
             // Do not send the profile if awsProfile is null or default
-            String profile = awsProfile==null || awsProfile.equals(AWSProfileHandler.DEFAULT_PROFILE) ? "" :
+            String profile = awsProfile == null || awsProfile.equals(AWSProfileHandler.DEFAULT_PROFILE) ? "" :
                     String.format("--profile %s ", awsProfile);
             Process process = Runtime.getRuntime().exec(String.format(
                     "%s codeartifact get-authorization-token %s--domain %s --domain-owner %s --query authorizationToken --output text",
@@ -25,9 +24,9 @@ class AWSInvoker {
             ProcessReader inputReader = new ProcessReader(process.getInputStream());
             ProcessReader errorReader = new ProcessReader(process.getErrorStream());
             while (!process.waitFor(100, TimeUnit.MILLISECONDS)) {
-                 if (cancellable.isCancelled()) {
-                     process.destroy();
-                 }
+                if (cancellable.isCancelled()) {
+                    process.destroy();
+                }
             }
             if (0 == process.exitValue()) {
                 ret.output = inputReader.getOutput();
@@ -40,16 +39,16 @@ class AWSInvoker {
                 ret.output = errorReader.getOutput();
             }
 
-        } catch (Exception ex){
+        } catch (Exception ex) {
             ret.output = "Error executing aws:" + ex.getMessage();
         }
         return ret;
     }
 
     private static class ProcessReader implements Runnable {
-        private InputStream inputStream;
-        private Thread thread;
-        private StringBuffer read = new StringBuffer();
+        private final InputStream inputStream;
+        private final Thread thread;
+        private final StringBuffer read = new StringBuffer();
 
         public ProcessReader(InputStream inputStream) {
             this.inputStream = inputStream;
@@ -57,13 +56,13 @@ class AWSInvoker {
             this.thread.start();
         }
 
-        public String getOutput(){
+        public String getOutput() {
             try {
                 this.thread.join();
-            } catch (InterruptedException ex){
+            } catch (InterruptedException ex) {
                 // thread interrupted, app being stopped, nothing else to do here
             }
-            return read.length()==0 ? null : read.toString();
+            return read.length() == 0 ? null : read.toString();
         }
 
         @Override
@@ -71,7 +70,7 @@ class AWSInvoker {
             new BufferedReader(new InputStreamReader(inputStream)).lines().forEach(this::readLine);
         }
 
-        private void readLine(String line){
+        private void readLine(String line) {
             if (!line.isEmpty()) {
                 read.append(line);
             }
