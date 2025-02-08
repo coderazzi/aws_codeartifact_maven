814dc25968433b3e744ddacfa8ee55de3ab12685
coderazzi <coderazzi@gmail.com>
2025-02-08 14:48:17 +0100

    Revert "Added support for SSO"
    This reverts commit f527961c3e50d0b4149e5ff8a0dbadc5d5c7a631.

--- src/main/java/net/coderazzi/aws_codeartifact_maven/gui/GenerationDialog.java
+++ src/main/java/net/coderazzi/aws_codeartifact_maven/gui/GenerationDialog.java
@@ -7,7 +7,10 @@ import com.intellij.openapi.ui.*;
 import com.intellij.ui.components.JBLabel;
 import com.intellij.util.ui.UIUtil;
 import net.coderazzi.aws_codeartifact_maven.state.AwsConfiguration;
-import net.coderazzi.aws_codeartifact_maven.utils.*;
+import net.coderazzi.aws_codeartifact_maven.utils.AWSInvoker;
+import net.coderazzi.aws_codeartifact_maven.utils.MavenSettingsFileHandler;
+import net.coderazzi.aws_codeartifact_maven.utils.MfaCodeValidator;
+import net.coderazzi.aws_codeartifact_maven.utils.OperationException;
 import net.coderazzi.aws_codeartifact_maven.state.Configuration;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -25,7 +28,7 @@ import java.util.TreeMap;
 import static com.intellij.util.ui.JBUI.Borders.empty;
 
 @SuppressWarnings({"unchecked"})
-public class GenerationDialog extends DialogWrapper {
+class GenerationDialog extends DialogWrapper implements AWSInvoker.BackgroundController {
 
     public static final String BACK_TEXT = "Back";
 
@@ -42,8 +45,7 @@ public class GenerationDialog extends DialogWrapper {
     private final String awsPath;
     private final Map<String, ConfigurationRow> configurations = new TreeMap<>();
     private final boolean isGenerateForAll;
-    private boolean completed, closeDialog;
-    private volatile boolean cancelled;
+    private boolean cancelled, completed, closeDialog;
 
     public GenerationDialog(final Project project,
                             final Configuration state) {
@@ -77,14 +79,6 @@ public class GenerationDialog extends DialogWrapper {
                 }
             }
         });
-    }
-
-    public boolean isCancelled(){
-        return cancelled;
-    }
-
-    public Project getProject() {
-        return project;
     }
 
     private void launch(){
@@ -124,16 +118,8 @@ public class GenerationDialog extends DialogWrapper {
                     mavenSettingsFileHandler.locateServer(configuration.mavenServerId);
                     if (!cancelled) {
                         setMessage(messageField, state, "Obtaining AWS Auth Token");
-                        InvokerController controller = new InvokerController(this) {
-                            @Override
-                            public void showMessage(String message) {
-                                if (!cancelled) {
-                                    setMessage(messageField, TaskState.RUNNING, message);
-                                }
-                            }
-                        };
-                        String token = new AWSInvoker(controller).getAuthToken(configuration.domain, configuration.domainOwner,
-                                awsPath, configuration.profile, configuration.region);
+                        String token = AWSInvoker.getAuthToken(configuration.domain, configuration.domainOwner,
+                                awsPath, configuration.profile, configuration.region, this);
                         if (!cancelled) {
                             setMessage(messageField, state, "Updating settings file");
                             mavenSettingsFileHandler.setPassword(token);
@@ -281,6 +267,30 @@ public class GenerationDialog extends DialogWrapper {
     @Override
     public boolean isOK() {
         return super.isOK() && completed;
+    }
+
+    @Override
+    public String requestMfaCode(String request)  throws OperationException{
+        final String []ret = new String[1];
+        try {
+            SwingUtilities.invokeAndWait(() -> {
+                Messages.InputDialog dialog = new Messages.InputDialog(project, request, "AWS input request", null, "", new MfaCodeValidator());
+                if (dialog.showAndGet()) {
+                    ret[0] = dialog.getInputString();
+                }
+            });
+        } catch(Exception iex) {
+            throw new OperationException("Internal plugin error");
+        }
+        if (ret[0] == null || ret[0].isEmpty()) {
+            throw new OperationException("No MFA code provided");
+        }
+        return ret[0];
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
     }
 
     private static void checkNotEmptyString(String text, String description) throws OperationException {
